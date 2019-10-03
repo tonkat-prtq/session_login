60da897f8288c0e208be4b0534b06ff812b78039
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-10-03 23:55:39 +0900

    メールアドレスのバリデーションに正規表現を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 } # nameカラムの値が存在すること（空文字でないこと）を検証する
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
