714501ca02a4bc22062b564b147272b441b92d0d
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-10-05 23:25:01 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :show]
+  resources :users
 end
