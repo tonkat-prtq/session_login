219235b094ee277526c2e4466db276beabc1da02
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-10-06 10:36:17 +0900

    routeにdestroyをセッティングしてなかった

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
   def destroy
     session.delete(:user_id)
     flash[:notice] = 'ログアウトしました'
-    redirect_to new_sessions_path
+    redirect_to new_session_path
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :show, :destroy]
   resources :users
 end
