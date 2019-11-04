1be189a03f57326416c767428b250e65c5c1484b
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-05 08:48:58 +0900

    viewの整理

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def new
@@ -7,10 +8,17 @@ class BlogsController < ApplicationController
   end
 
   def create
-    Blog.create(blog_params)
-    redirect_to new_blog_path
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました"
+    else
+      render :new
+    end
   end
 
+  def show
+    @blog = Blog.find(params[:id])
+  end
   private
 
   def blog_params
