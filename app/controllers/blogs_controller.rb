971f4ba172e025004e50aca725483286b0f1561c
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-05 07:18:21 +0900

    strong paramater実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,20 @@
 class BlogsController < ApplicationController
   def index
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    redirect_to new_blog_path
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  
 end
