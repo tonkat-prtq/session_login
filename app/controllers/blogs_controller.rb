54fa65c1aa680b6ddeb30edb6118029072c15dea
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-06 07:28:05 +0900

    基本的ブログ機能の作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
     @blogs = Blog.all
   end
@@ -9,20 +10,48 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path, notice: "ブログを作成しました"
-    else
+    if params[:back]
       render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました"
+      else
+        render :new
+      end
     end
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログを削除しました"
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
+  end
+
   private
 
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
 
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
   resources :sessions, only: [:new, :create, :show, :destroy]
   resources :users
 end
